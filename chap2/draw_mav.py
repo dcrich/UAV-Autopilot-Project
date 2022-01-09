7fc109724f9bb590e049257402dfc314428e3f05
dcrich <43016577+dcrich@users.noreply.github.com>
2022-01-08 17:48:33 -0700

    Points and meshes added

--- chap2/draw_mav.py
+++ chap2/draw_mav.py
@@ -91,10 +91,28 @@ class DrawMav:
 
         # points are in NED coordinates
         # define the points on the aircraft following diagram Fig 2.14
+        # points = np.array([[0, 0, 0],  # point 1 [0]
+        #                    [1, 1, 1],  # point 2 [1]
+        #                    [1, 1, 0],  # point 3 [2]
+        #                    ]).T
         points = np.array([[0, 0, 0],  # point 1 [0]
-                           [1, 1, 1],  # point 2 [1]
-                           [1, 1, 0],  # point 3 [2]
+                           [-(fuse_l1 - fuse_l2), 0.5 * fuse_w, -0.5 * fuse_h],  # point 2 [1]
+                           [-(fuse_l1 - fuse_l2), -0.5 * fuse_w, -0.5 * fuse_h],  # point 3 [2]
+                           [-(fuse_l1 - fuse_l2), -0.5 * fuse_w, 0.5 * fuse_h],  # point 4 [3]
+                           [-(fuse_l1 - fuse_l2), 0.5 * fuse_w, 0.5 * fuse_h],  # point 5 [4]
+                           [-(fuse_l1 + fuse_l3),0,0],  # point 6 [5]
+                           [-fuse_l1, 0.5*wing_w, 0],  # point 7 [6]
+                           [-(fuse_l1 + wing_l), 0.5 * wing_w, 0],  # point 8 [7]
+                           [-(fuse_l1 + wing_l), -0.5 * wing_w, 0],  # point 9 [8]
+                           [-fuse_l1, -0.5*wing_w, 0],  # point 10 [9]
+                           [-(fuse_l1 + fuse_l3 - tail_l), 0.5 * tail_w, 0],  # point 11 [10]
+                           [-(fuse_l1 + fuse_l3), 0.5 * tail_w, 0],  # point 12 [11]
+                           [-(fuse_l1 + fuse_l3), -0.5 * tail_w, 0],  # point 13 [12]
+                           [-(fuse_l1 + fuse_l3 - tail_l), -0.5 * tail_w, 0],  # point 14 [13]
+                           [-(fuse_l1 + fuse_l3 - tail_l), 0, 0],  # point 15 [14]
+                           [-(fuse_l1 + fuse_l3), 0, -tail_h],  # point 16 [15]
                            ]).T
+        
 
         # scale points for better rendering
         scale = 50
@@ -117,6 +135,20 @@ class DrawMav:
           (a rectangle requires two triangular mesh faces)
         """
         points = points.T
-        mesh = np.array([[points[0], points[1], points[2]]])
+        # mesh = np.array([[points[0], points[1], points[2]]])
+        mesh = np.array([[points[0], points[1], points[2]], # front 1
+                         [points[0], points[2], points[3]], # front 2
+                         [points[0], points[3], points[4]], # front 3
+                         [points[0], points[4], points[1]], # front 4
+                         [points[5], points[1], points[2]], # body 1
+                         [points[5], points[2], points[3]], # body 2
+                         [points[5], points[3], points[4]], # body 3
+                         [points[5], points[4], points[1]], # body 4
+                         [points[6], points[7], points[8]], # wing 1
+                         [points[6], points[9], points[8]], # wing 2
+                         [points[10], points[11], points[12]], # tail 1
+                         [points[10], points[13], points[12]], # tail 2
+                         [points[5], points[14], points[15]], # tail 3
+                        ])
 
         return mesh
